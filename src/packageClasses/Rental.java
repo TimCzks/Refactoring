ad90d70008d3504457eb4d4d7ee8408704c5f7a5
Tim Czerkas <t.handy@gmx.de>
2022-05-13 09:52:34 +0200

    Moved method to improve switch statement/Value usage

--- src/packageClasses/Rental.java
+++ src/packageClasses/Rental.java
@@ -10,31 +10,11 @@ public class Rental {
 	}
 
 	public double getCharge() {
-		double result = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
+		return movie.getCharge(daysRented);
 	}
 
 	public int getFrequentRenterPoints() {
-		// add bonus for a two day new release rental
-		if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1)
-			return 1;
-		else
-			return 0;
+		return movie.getFrequentRenterPoints(daysRented);
 	}
 
 	public int getDaysRented() {
--- src/packageTests/CustomerTest.java
+++ src/packageTests/CustomerTest.java
@@ -15,7 +15,7 @@ class CustomerTest {
 	@Test
 	void testStatement() {
 		String expectedResult = "Rental Record for Dummy\n\tTitle\t\tDays\tAmount\n" + "\t" + "title\t\t1\t2.0\n"
-				+ "Amount owed is " + 2.0 + "\n" + "You earned " + 0 + " frequent renter points";
+				+ "Amount owed is " + 2.0 + "\n" + "You earned " + 1 + " frequent renter points";
 		classUnderTest.addRental(new Rental(new Movie("title", Movie.REGULAR), 1));
 		assertEquals(expectedResult, classUnderTest.statement());
 	}
